539b86b96391d601fd803eb79627b0c26b58182a
ottojoash <louisjoshbricks@gmail.com>
2022-08-18 21:00:48 -0400

    server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
+//import connectDatabase from './config/database';
 const app = require('./app')
+const connectDatabase =require('./config/database')
 
 const dotenv = require('dotenv');
 
 // setting up config files
 
 dotenv.config({  path: 'backend/config/config.env'})
+
+// connecting to database
+//connectDatabase();
 
 app.listen(process.env.PORT, () =>{
     console.log('server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} node.')
