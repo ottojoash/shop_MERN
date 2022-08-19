8c48aa493d3d33b70787e0927aeb26ec5e058617
ottojoash <louisjoshbricks@gmail.com>
2022-08-19 08:10:59 -0400

    adminpdt

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,16 @@
+const Product = require('../models/product')
+
+//create new product => /api/v1/product/new
+exports.newPrdouct = async(req,res,next) => {
+
+    const product = await Product.create(req.body)
+
+    res.status(201).json({
+        sucess:true,
+        product
+    })
+}
+
 
 exports.getProducts =(req, res,next) =>{
     res.status(200).json({
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router();
 
 
-const {getProducts} = require('../controllers/productController')
+const {getProducts, newProducts } = require('../controllers/productController')
 
 
 router.route('/products').get(getProducts);
 
+router.route(/product/new).post(newProduct);
 
 module.exports = router;
